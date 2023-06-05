c3adf2b718d95f7023c3f78f3c277e0cf5d71e97
LBeul <l.beul@outlook.de>
2023-06-05 12:44:22 +0200

    add imagedata uuid & metadata dependency

--- app/src/main/java/com/lbeul/shotspots_v2/models/ImageData.java
+++ app/src/main/java/com/lbeul/shotspots_v2/models/ImageData.java
@@ -2,8 +2,10 @@ package com.lbeul.shotspots_v2.models;
 
 import java.nio.file.Path;
 import java.util.Date;
+import java.util.UUID;
 
 public class ImageData {
+    private UUID id;
     private Path imageUri;
     private GeoLocation location;
     private Date timeStamp;
